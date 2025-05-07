05026ac0e67020685f86c5ba90d76963d57e0b4d
Mohammed Samrose <samrose.mohammed@gmail.com>
2025-05-07 14:28:07 +0545

    minor changes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,11 @@
 import AppSidebar from "@/components/AppSideBar";
+import { DropDownForProfileNav } from "@/components/DropDownForProfileNav";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { authOptions } from "@/lib/auth";
 import { Bell, Moon, Sun, Github } from "lucide-react";
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 const Layout = async ({ children }: PropsWithChildren) => {
@@ -17,14 +19,25 @@ const Layout = async ({ children }: PropsWithChildren) => {
           <SidebarTrigger />
           <div className="flex items-center justify-center gap-6">
             <Bell size={20} />
-            <Github size={20} />
-            <Avatar className="w-8 h-8">
+            <Link
+              target="_blank"
+              className="cursor-pointer hover:bg-muted"
+              href={"https://github.com/samrosemohammed/next-cms"}
+            >
+              <Github size={20} />
+            </Link>
+            {/* <Avatar className="w-8 h-8">
               <AvatarImage
                 src={user?.image!}
                 alt={`${user?.name} profile picture`}
               />
               <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            </Avatar> */}
+            <DropDownForProfileNav
+              name={user?.name!}
+              image={user?.image!}
+              email={user?.email!}
+            />
           </div>
         </div>
         {children}
---
+++ src/components/DropDownForProfileNav.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useState } from "react";
+import { User, Settings, LogOut, HelpCircle, CreditCard } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+interface DropDownForProfileNavProps {
+  name: string;
+  image: string;
+  email: string;
+}
+export const DropDownForProfileNav = ({
+  name,
+  image,
+  email,
+}: DropDownForProfileNavProps) => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const handleLogOut = () => {
+    signOut({
+      callbackUrl: "/login",
+    });
+  };
+  const navigateTo = (path: string) => {
+    router.push(path);
+  };
+  return (
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="relative h-8 w-8 rounded-full hover:bg-muted"
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage
+              src={image || "/placeholder.svg?height=32&width=32"}
+              alt={`${name} profile picture`}
+            />
+            <AvatarFallback>UN</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{name}</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => navigateTo("/dashboard/settings")}
+          >
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="cursor-pointer"
+            onClick={() => navigateTo("/dashboard/settings")}
+          >
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer">
+            <HelpCircle className="mr-2 h-4 w-4" />
+            <span>Help</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={handleLogOut}
+          className="cursor-pointer text-rose-500 focus:text-rose-500 focus:bg-rose-50 dark:focus:bg-rose-950/10"
+        >
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
