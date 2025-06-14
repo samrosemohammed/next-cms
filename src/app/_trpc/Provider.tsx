aa50f79f8a4165cbf2b986c47f5ca19c1d9511ef
samrosemohammed <samrose.mohammed@gmail.com>
2025-06-14 23:13:16 +0545

    trpc batch link

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -10,7 +10,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: "https://cms.mohammedsamrose.com.np/api/trpc",
         }),
       ],
     })
