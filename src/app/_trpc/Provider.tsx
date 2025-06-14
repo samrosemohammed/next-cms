59cdf066583a6feb52752bbf18b1bfbe41c4c792
samrosemohammed <samrose.mohammed@gmail.com>
2025-06-14 16:16:46 +0545

    trpc config

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -10,7 +10,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: process.env.TRPC_API_URL || "http://localhost:3000/api/trpc",
         }),
       ],
     })
